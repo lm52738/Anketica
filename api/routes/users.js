7cdb8bb390c021f7642c7b5dc70d91769c09adc3
Hrvoje Hemen <hrvojehemen@gmail.com>
2022-01-26 14:24:01 +0100

    query za sve osobe

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const db = require("../db/index.js");
+const bodyParser = require("body-parser");
+router.use(bodyParser.json({type: "application/*+json"}));
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+    let rez = await getAllUsers();
+    res.json(rez.rows);
 });
+
+
+let getAllUsers = async function () {
+    return db.query(
+        `SELECT ime, prezime, mail
+         from osobe`
+    );
+};
+
 
 module.exports = router;
